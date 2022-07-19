4f39b57858e55b586b813bf6679dcbbdefd5be85
als8921 <96395884+als8921@users.noreply.github.com>
2022-07-19 20:31:46 +0900

    Update PWM_Control.py

--- PWM_Control.py
+++ PWM_Control.py
@@ -13,7 +13,7 @@ P_gain = 5
 D_gain = 3
 
 tau_N = 0
-tau_X = 150
+tau_X = 300
 isEnd = False
 def callback(data1, data2):
     global psi_error_past
@@ -36,8 +36,8 @@ def callback(data1, data2):
 
     tau_N = P_gain * psi_error + D_gain * psi_error_dot
 
-    if(tau_N > 2 * tau_X): tau_N = 2 * tau_X
-    if(tau_N < -2 * tau_X): tau_N = -2 * tau_X
+    # if(tau_N > 2 * tau_X): tauN = 2 * tau_X
+    # if(tau_N < -2 * tau_X): tau_N = -2 * tau_X
 
     Rpwm = tau_X - tau_N / 2 + 1500
     Lpwm = tau_X + tau_N / 2 + 1500
@@ -46,29 +46,27 @@ def callback(data1, data2):
     if(isEnd):
         Lpwm, Rpwm = 1500, 1500
 
-    if(Rpwm > 1850): Rpwm = 1850
-    elif(Rpwm < 1150) : Rpwm = 1150
+    if(Rpwm > 1950): Rpwm = 1950
+    elif(Rpwm < 1050) : Rpwm = 1050
 
-    if(Lpwm > 1850): Lpwm = 1850
-    elif(Lpwm < 1150) : Lpwm = 1150    
+    if(Lpwm > 1950): Lpwm = 1950
+    elif(Lpwm < 1050) : Lpwm = 1050    
     
 
 
-    Rpwm = 3000 - Rpwm
-
+    Lpwm = 3000 - Lpwm
     PWM_data = Float64MultiArray()
     PWM_data.data = [Lpwm, Rpwm]
     print(data1.data, data2.data, Lpwm, Rpwm)
     pub.publish(PWM_data)
     timepast = time.time()
-
     
 
 
 if __name__ == '__main__':
     rospy.init_node('PPPWM', anonymous=False)
-    sub1 = message_filters.Subscriber("/IMUdata", Float32)
-    sub2 = message_filters.Subscriber("/PPsi_d", Float32)
+    sub1 = message_filters.Subscriber("/IMUData", Float32)
+    sub2 = message_filters.Subscriber("/Psi_d", Float32)
     mf = message_filters.ApproximateTimeSynchronizer([sub1, sub2],10,0.1,allow_headerless=True)
     mf.registerCallback(callback)
 
